1c59a8da27b41bc170f8760783e93b53028f5953
Shailesh Dadhich <kesh4v50@gmail.com>
2026-09-15 04:52:51 +0530

    Updated js file with deployed backend link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* ============================================================
    Config
    ============================================================ */
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://ml-mental-health-score-un54.onrender.com";
 const PREDICT_ENDPOINT = `${API_BASE_URL}/predict`;
 
 // The backend returns a raw regression value. We don't know the
